9856205f4a8b7383dea1684de8249249fb8456fe
Venugopal Bhatia <venugopalbhatia@gmail.com>
2021-04-09 15:15:31 +0530

    setup views

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const port = 8000;
 
 const app = express();
 
-
-
+app.set('view engine','ejs');
+app.set('views','./views')
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const home = require('../controllers/home');
+
+const router = express.Router();
+
+router.get('/',home.homeController);
+router.use('/query',require('./queryPage'));
+
+
+
+
+module.exports = router;
