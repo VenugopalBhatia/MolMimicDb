66710c35fe52ad0d5f1968ff2ada5dedee907b8b
Venugopal Bhatia <venugopalbhatia@gmail.com>
2021-06-04 15:11:01 +0530

    change email contact

--- controllers/mailer/emailResults.js
+++ controllers/mailer/emailResults.js
@@ -10,7 +10,7 @@ exports.sendCSVAsAttachment = async function(query_details,queryResult,user_emai
         var csv = await getCSVData(queryResult,query_details['count']);
         var HTMLString = nodemailer.renderTemplate(query_details,'/emailCSVData.ejs')
         nodemailer.transporter.sendMail({
-            from: '"ImitateDB Team" dbimitate@gmail.com',
+            from: '"The ImitateDB Team" dbimitate@gmail.com',
             to: user_email,
             subject: 'The Data you requested from Imitate Db',
             html:HTMLString,
